58a247f20ff876cb1c929175a5c2fa8231723064
mdhuggins <mdhuggins@optonline.net>
2014-11-09 12:22:56 -0500

    Small Changes

--- databasetest.php
+++ databasetest.php
@@ -1,5 +1,5 @@
 <?php
-	mysql_connect('50.62.209.5','Pgma14','Geogre333') or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
-	mysql_select_db('Pgma14');
+	mysql_connect('50.62.209.5','User','Password') or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
+	mysql_select_db('User');
 	mysql_query("INSERT INTO Laws VALUES('Test',00000000000,'None','01/01/01','None','None')");
 ?>
--- insert.php
+++ insert.php
@@ -14,14 +14,11 @@
 			echo("it works");
 		}
 		else{
-			echo("fuck you");
+			echo("oh no");
 		}
-		
+
 	}
 
-	//mysql_connect('50.62.209.5','Pgma14','Geogre333') or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
-	//mysql_select_db('Pgma14');
-//mysql_query("INSERT INTO Laws VALUES('$lawtitle','$lawtrackingnumber','$category','$datesigned','$tags','$billtext')");
 
 	$username=$_POST["username"];
 	$password = $_POST["pwd"];
