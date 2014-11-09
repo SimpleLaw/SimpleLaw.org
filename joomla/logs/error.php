40d88e1f834da23727056ffd36be524737fabd8b
mdhuggins <mdhuggins@optonline.net>
2014-11-08 22:18:31 -0500

    Sorting, Removed extra files

--- joomla/logs/error.php
+++
@@ -1,8 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2014-07-24 18:24:47 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority	category	message
-2014-07-24T18:24:46+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
-2014-07-24T18:26:04+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
--- options/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$responseText=file_get_contents("http://api.legiscan.com/?key=9635a55e3fb637fb7ce2dc3810fa2833&op=getMasterList&state=CT");
-$parsed=explode(",",explode(":",$responseText,9)[7])[0];
-echo(file_get_contents("http://api.legiscan.com/?key=9635a55e3fb637fb7ce2dc3810fa2833&op=getBill&id=".$parsed));
-?>
